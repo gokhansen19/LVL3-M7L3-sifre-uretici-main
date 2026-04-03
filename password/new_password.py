fb7567d4f88e040a7f4e4cc6208a0b6378f1e0e3
YusufBera <zesert2141@gmail.com>
2026-04-03 20:59:42 +0300

    test

--- password/new_password.py
+++ password/new_password.py
@@ -1,14 +1,18 @@
 import random
 import string
+import pytest
 
 def generate_password(length=12):
-    """Belirtilen uzunlukta rastgele bir şifre oluşturur."""
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''
     for i in range(length):
         password += random.choice(characters)
     return password
 
-# Kullanım örneği
-password_length = 12  # İstediğiniz herhangi bir şifre uzunluğunu seçebilirsiniz
-print("Yeni şifreniz:", generate_password(password_length))
+def test_generate_password():
+    assert len(generate_password(8)) == 8
+    assert generate_password() != generate_password()
+    assert isinstance(generate_password(), str)
+
+if __name__ == "__main__":
+    print("Yeni şifreniz:", generate_password(12))
